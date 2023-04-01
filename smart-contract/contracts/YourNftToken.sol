ca4c0140d54ae46917ef68466cd941d623245a46
Stacy <58567154+addyevdox@users.noreply.github.com>
2023-04-01 11:02:05 -0300

    Update YourNftToken.sol

--- smart-contract/contracts/YourNftToken.sol
+++ smart-contract/contracts/YourNftToken.sol
@@ -6,7 +6,7 @@ import 'erc721a/contracts/ERC721A.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
-
+// Custome smart cnotract
 contract YourNftToken is ERC721A, Ownable, ReentrancyGuard {
 
   using Strings for uint256;
